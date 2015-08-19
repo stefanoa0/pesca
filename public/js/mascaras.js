ca9996b1cfc0ce822c5d2574f7ba788495664fe7
stefanoa0 <stefanouesc@gmail.com>
2015-08-19 11:09:02 -0300

    Correção pesqueiros

--- public/js/mascaras.js
+++ public/js/mascaras.js
@@ -1,14 +1,9 @@
 $(document).ready(function($){
-    $(".telefone").mask("(99) 9999-9999");
-    $("#cep").mask("99999-999");
-    $("#cpf").mask("999.999.999-99");
-    $('input[id*=data_]').mask("99/99/9999");
-    $('input[id*=hora_]').mask("99:99");
+    $(".telefone").mask("(99)99999-9999");
+    $(".cep").mask("99999-999");
+    $(".cpf").mask("999.999.999-99");
+    $(".cnpj").mask("99.999.999/9999-99");
+    $('.data').mask("99/99/9999");
+    $('.time').mask("99:99");
     
-    $("#Submit input").click(function() {
-        $(".telefone").mask("9999999999");
-        $("#cep").mask("99999999");
-        $("#cpf").mask("99999999999");
-        return true;
-    });
 });
